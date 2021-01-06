11b7bf277e74350900ba58971a0a9c62ba6acd67
JOD <tj1998g@gmail.com>
2021-01-06 13:25:50 +0500

    lesson18-1
    lesson18-1

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 'use strict';
 
-class Appear {
-	constructor(divElem, inputTextElem, pElem) {
-		this.divElem = document.querySelector(divElem);
-		this.inputTextElem = document.querySelector(inputTextElem);
-		this.pElem = document.querySelector(pElem);
+class Throttling {
+	constructor(divThrottlingElem, inputThrottlingTextElem, pThrottlingElem) {
+		this.divElem = document.querySelector(divThrottlingElem);
+		this.inputTextElem = document.querySelector(inputThrottlingTextElem);
+		this.pElem = document.querySelector(pThrottlingElem);
 	}
 	style() {
 		this.divElem.style.border = '1px solid silver';
 		this.divElem.style.padding = '10px';
+		this.divElem.style.marginBottom = '10px';
 		this.pElem.style.border = '1px solid silver';
 		this.pElem.style.padding = '10px';
 	}
 	assignment() {
 		this.pElem.textContent = this.inputTextElem.value;
 	}
-	throttle(func) {
+	throttling(func, ms) {
 		let isThrottled = false,
 			savedArgs,
 			savedThis;
@@ -33,15 +34,47 @@ class Appear {
 					wrapper.apply(savedThis, savedArgs);
 					savedArgs = savedThis = null;
 				}
-			}, 1000);
+			}, ms);
 		};
 		return wrapper;
 	}
 	init() {
 		this.style();
-		const delay = this.throttle(this.assignment.bind(this));
+		const delay = this.throttling(this.assignment.bind(this), 1000);
 		this.inputTextElem.addEventListener('input', delay);
 	}
 }
-const appear = new Appear('.div', '.input-text', '.p');
-appear.init();
+const throttling = new Throttling('.wrap__throttling', '.input__text-throttling', '.p__throttling');
+throttling.init();
+
+class Debouncing {
+	constructor(divDebouncingElem, inputDebouncingTextElem, pDebouncingElem) {
+		this.divElem = document.querySelector(divDebouncingElem);
+		this.inputTextElem = document.querySelector(inputDebouncingTextElem);
+		this.pElem = document.querySelector(pDebouncingElem);
+	}
+	style() {
+		this.divElem.style.border = '1px solid silver';
+		this.divElem.style.padding = '10px';
+		this.pElem.style.border = '1px solid silver';
+		this.pElem.style.padding = '10px';
+	}
+	assignment() {
+		this.pElem.textContent = this.inputTextElem.value;
+	}
+	debouncing(func, ms) {
+		let timeout;
+		return function() {
+			const fnCall = () => { func.apply(this, arguments); };
+			clearTimeout(timeout);
+			timeout = setTimeout(fnCall, ms);
+		};
+	}
+	init() {
+		this.style();
+		const delay = this.debouncing(this.assignment.bind(this), 500);
+		this.inputTextElem.addEventListener('input', delay);
+	}
+}
+const debouncing = new Debouncing('.wrap__debouncing', '.input__text-debouncing', '.p__debouncing');
+debouncing.init();
